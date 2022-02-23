9039b024c491488240b401883aa372e7b79228d7
James Robertson <digital.robertson@gmail.com>
2022-02-23 19:24:51 +0000

    bug fix: Replaced  RXFHelper with RXFReader.

--- domle.gemspec
+++ domle.gemspec
@@ -1,15 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'domle'
-  s.version = '0.5.3'
+  s.version = '0.6.0'
   s.summary = 'Domle (DOM + Rexle) is the document object model used by the Svgle gem'
   s.authors = ['James Robertson']
   s.files = Dir['lib/domle.rb']
-  s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.6')
+  s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.14')
   s.add_runtime_dependency('csslite', '~> 0.2', '>=0.2.0')
-  s.add_runtime_dependency('rxfhelper', '~> 1.0', '>=1.0.0')
+  s.add_runtime_dependency('rxfreader', '~> 0.2', '>=0.2.1')
   s.signing_key = '../privatekeys/domle.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/domle'
 end
--- lib/domle.rb
+++ lib/domle.rb
@@ -4,7 +4,7 @@
 
 require 'rexle'
 require 'csslite'
-require 'rxfhelper'
+require 'rxfreader'
 
 
 class Style < Hash
@@ -15,32 +15,32 @@ class Style < Hash
     super().merge! h
 
   end
-  
+
   def []=(k,v)
 
     super(k,v)
     @parent[:style] = self.map{|x| x.join(':') }.join(';')
-    
+
     if @parent.callback and @parent.callback.respond_to? :refresh then
-      @parent.callback.refresh 
+      @parent.callback.refresh
     end
-    
+
   end
 end
 
 class VisualAttributes < Attributes
-  
+
   attr_reader :callback
-  
+
   def initialize(x, parent: nil)
     @callback = parent
     self.merge! x if x
-  end  
-  
+  end
+
   def style(parent=nil)
-    
+
     @callback ||= parent
-    
+
     if @style.nil? then
 
       h = self[:style].split(';').inject({}) do |r, x|
@@ -53,52 +53,53 @@ class VisualAttributes < Attributes
     end
     @style
   end
-  
+
 end
 
 class Domle < Rexle
   using ColouredText
-  
+
   class Element < Rexle::Element
-    
+
 
     class ClassList
-      
+
       def initialize(a, rexle, node)
         @a, @rexle, @node = a, rexle, node
       end
-      
+
       def toggle(name)
         @a.include?(name) ? @a.delete(name) : @a << name
         puts '@rexle?: ' + @rexle.inspect if @debug
         @rexle.refresh_css(@node) if @rexle
-      end        
-    end    
+      end
+    end
 
     @default = {}
     @attr_map = {}
     attr_reader :attr_map
-    
+
     def initialize(name=self.class.to_s.downcase[/\w+$/], value: nil, \
                           attributes: {}, rexle: nil)
-      
+
       attributes.merge!(style: '') unless attributes.has_key? :style
-      super(name, value: value, attributes: VisualAttributes.new(attributes), rexle: rexle)
-      
+      super(name, value: value, attributes: VisualAttributes.new(attributes),
+            rexle: rexle)
+
     end
-    
+
     def self.attr2_accessor(*a)
 
       a.concat %i(id transform)
-      # DOM events      
-      a.concat %i(onload onmousemove onmousedown onmouseenter 
-      onmouseleave onclick onscroll onkeydown onkeyup) 
-      
+      # DOM events
+      a.concat %i(onload onmousemove onmousedown onmouseenter
+      onmouseleave onclick onscroll onkeydown onkeyup)
+
       a.each do |attribute|
 
         class_eval do
 
-          define_method attribute.to_s.gsub('-','_').to_sym do 
+          define_method attribute.to_s.gsub('-','_').to_sym do
             attributes[attribute]
           end
 
@@ -111,53 +112,53 @@ class Domle < Rexle
 
         end
       end
-      
+
       def rexle?()
         not @rexle.nil?
       end
-      
+
       def rexle()
         @rexle
       end
-            
+
     end
-    
+
     def self.attr_mapper(h)
       @attr_map = h
     end
-    
+
     def active?()
       @active
     end
-    
+
     def active=(v)
       @active = v
-    end    
-    
+    end
+
     def classlist()
-      
+
       a = attributes[:class]
       cl = ClassList.new(a, @rexle, self)
-      
 
-      
-      
+
+
+
       return cl
     end
-    
+
     def style()
       attributes.style(@rexle)
     end
-    
-    
+
+
     def hotspot?(x,y)
 
       (boundary.first.is_a?(Array) ? boundary : [boundary]).all? do |box|
         x1, y1, x2, y2 = box
         (x1..x2).include? x and (y1..y2).include? y
       end
-    end    
-    
+    end
+
     def obj=(v)
       @obj = v
     end
@@ -165,27 +166,27 @@ class Domle < Rexle
     def obj()
       @obj
     end
-    
-    
+
+
     private
-    
+
     def attr_update(name, val)
       return unless @attr_map
       puts 'inside attr_update, @attr_map: ' + @attr_map.inspect if @debug
-      name = (@attr_map[name].to_s + '=').to_sym      
+      name = (@attr_map[name].to_s + '=').to_sym
       @obj.method(name).call(val) if @obj.respond_to? name
     end
   end
-  
+
   class Script < Element
 
-  end  
-  
+  end
+
   class Style < Element
-    
+
 
   end
-  
+
   attr_reader :event
 
   def initialize(src='<svg/>', callback: nil, rexle: nil, debug: false)
@@ -193,14 +194,14 @@ class Domle < Rexle
     @callback, @debug, @rexle = callback, debug, rexle
     puts 'inside Domle initialize'.info if @debug
     puts 'src:' + src.inspect if @debug
-    
-    super(RXFHelper.read(src).first, rexle: self)
+
+    super(RXFReader.read(src).first, rexle: self)
     @debug = debug
-    
+
     puts 'before find add_css' if @debug
     find_add_css() if src
 
-    
+
     @event = {
       keydown: [],
       keyup: [],
@@ -210,47 +211,47 @@ class Domle < Rexle
       mouseleave: [],
       scroll: []
     }
-    
-  end  
-  
-  def addevent_listener(event, method_name)
-        
-    @event[event.to_sym] << method_name
-    
+
   end
-    
-  
+
+  def addevent_listener(event, method_name)
+
+    @event[event.to_sym] << method_name
+
+  end
+
+
   def element_by_id(id)
     @doc.root.element("//*[@id='#{id}']")
   end
-  
+
   def elements_by_name(name)
     @doc.root.xpath("//*[@name='#{name}']")
-  end  
-    
+  end
+
   def elements_by_tagname(name)
     @doc.root.xpath("//#{name}")
-  end    
-   
+  end
+
   def refresh()
     @callback.refresh if @callback and @callback.respond_to? :refresh
   end
-  
+
   def refresh_css(node)
     find_add_css(node.parent)
   end
-  
+
   protected
-  
+
   def add_default_css()
   end
-  
+
   def add_external_css(node=nil)
-    
+
     # check for an external CSS file
     if @instructions and @instructions.any? then
 
-      hrefs = @instructions.inject([]) do |r,x| 
+      hrefs = @instructions.inject([]) do |r,x|
 
         if x[0] =~ /xml-stylesheet/i and x[1] =~ /text\/css/i then
 
@@ -259,28 +260,28 @@ class Domle < Rexle
           r
         end
       end
-      
+
       add_css hrefs.map{|x| RXFHelper.read(x).first}.join, node
-      
+
     end
-    
+
   end
-  
+
   def add_inline_css(node=nil)
     puts 'inside add_inline_css' if @debug
-    @doc.root.xpath('//style').each {|e|  add_css e.text.to_s, node } 
+    @doc.root.xpath('//style').each {|e|  add_css e.text.to_s, node }
   end
-  
+
   def find_add_css(node=nil)
-    
+
     puts 'inside find_add_css' if @debug
     add_default_css() unless node
     add_inline_css(node)
     add_external_css(node)
-  end    
-  
+  end
+
   private
-  
+
   def add_css(s, node=@doc, override: true)
 
     puts 'add_css s: ' + s.inspect if @debug
@@ -295,9 +296,9 @@ class Domle < Rexle
   #
   def defined_elements()
     {
-      doc: Rexle::Element      
+      doc: Rexle::Element
     }
-  end  
+  end
 
   def scan_element(name, attributes=nil, *children)
 
@@ -312,35 +313,35 @@ class Domle < Rexle
     if children then
 
       children.each do |x|
-        
+
         if x.is_a? Array then
 
-          element.add_element scan_element(*x)        
-          
+          element.add_element scan_element(*x)
+
         elsif x.is_a? String then
 
           if x.is_a? String  then
 
             element.add_element(x) if x.strip.length > 0
-            
+
           elsif x.name == '![' then
 
             e = Rexle::CData.new(x)
             element.add_element e
-            
+
           elsif x.name == '!-' then
 
             e = Rexle::Comment.new(x)
             element.add_element e
-            
+
           end
-          
+
         end
       end
     end
-    
+
     return element
   end
-  
-    
+
+
 end
